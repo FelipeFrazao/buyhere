7cf35df55ea87b20569d8b20e6a46e49a188ba8c
felipe-frazao <felipe.frazao@al.infnet.edu.br>
2017-10-25 21:42:33 -0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'bh-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bh';
+  title = 'Buy Here';
 }
